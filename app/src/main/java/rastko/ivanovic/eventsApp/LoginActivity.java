579e7260c8ddd5a634b5c116ffa0216e826a5c31
Rastko9 <rastkoivanovicftn@gmail.com>
2026-04-15 21:21:28 +0200

    95% done LoginActivity & created EventsActivity

--- app/src/main/java/rastko/ivanovic/eventsApp/LoginActivity.java
+++ app/src/main/java/rastko/ivanovic/eventsApp/LoginActivity.java
@@ -1,7 +1,9 @@
 package rastko.ivanovic.eventsApp;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -21,5 +23,29 @@ public class LoginActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        Button btnLogin = findViewById(R.id.btn_login);
+        Button btnRegister = findViewById(R.id.btn_register);
+        LinearLayout buttonContainer = findViewById(R.id.btn_container);
+
+        btnLogin.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                buttonContainer.setVisibility(View.INVISIBLE);
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new LoginFragment()).commit();
+            }
+        });
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                buttonContainer.setVisibility(View.INVISIBLE);
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new RegisterFragment()).commit();
+            }
+        });
+
     }
 }
